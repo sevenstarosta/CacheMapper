ddbd31c974b1f58e591e7240577c591ecb6776ef
Seven Starosta <sevenstarosta@gmail.com>
2017-11-15 11:26:05 -0500

    fixed some layout, added camera

--- app/src/main/java/com/cachemapper/addcache.java
+++ app/src/main/java/com/cachemapper/addcache.java
@@ -7,8 +7,11 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -22,6 +25,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -30,15 +34,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class addcache extends AppCompatActivity {
     public static final String PREFS_NAME = "CacheMapperPrefsFile";
     private static final int TAKE_PHOTO_PERMISSION = 1;
+    static final int REQUEST_TAKE_PHOTO = 2;
+
+    Uri file;
+
+    ImageView imageView;
+    Button takePictureButton;
 
     private DatabaseReference mDatabase;
     private LocationManager locationManager;
     private LocationListener locationListener;
 
     public Button backButton;
+    private EditText nameEditText;
+    private EditText descEditText;
 
     Double currentLat;
     Double currentLon;
@@ -47,7 +63,11 @@ public class addcache extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcache);
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        descEditText = (EditText)findViewById(R.id.descEditText);
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        takePictureButton = (Button) findViewById(R.id.takePictureButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         if ( Build.VERSION.SDK_INT >= 23 &&
                 ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
@@ -55,11 +75,9 @@ public class addcache extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION }, TAKE_PHOTO_PERMISSION);
         }
 
-        // Add code here to register the listener with the Location Manager to receive location updates
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                // Add code here to do stuff when the location changes
                 currentLat = location.getLatitude();
                 currentLon = location.getLongitude();
             }
@@ -70,6 +88,7 @@ public class addcache extends AppCompatActivity {
 
             public void onProviderDisabled(String provider) {}
         };
+
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
         backButton = (Button) findViewById(R.id.backbutton);
@@ -81,18 +100,67 @@ public class addcache extends AppCompatActivity {
                 finish();
             }
         });
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // This is called when permissions are either granted or not for the app
+
+        if (requestCode == TAKE_PHOTO_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                takePictureButton.setEnabled(true);
+            }
+        }
+    }
+
+    public void takePicture(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Add code here to handle results from both taking a picture or pulling
+        // from the image gallery.
+
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            imageView.setImageURI(file);
+        }
     }
 
     public void uploadFirebaseData(View view) {
         //Check validity of data first!
         mDatabase = FirebaseDatabase.getInstance().getReference();
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        String name="a";
-        String description="b";
+        String name= nameEditText.getText().toString();
+        String description= descEditText.getText().toString();
         double latitude = currentLat.doubleValue();
         double longitude = currentLon.doubleValue();
         String username = settings.getString("currentUser",null);
         cacheLocation cache = new cacheLocation(username,name,description,latitude,longitude);
+        //currently overwrites...
         mDatabase.child("caches").child(name).setValue(cache);
     }
 
